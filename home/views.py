0fca5d84f46f2839ee387377bdf617cc318b409f
Charles Enright <c.enright@me.com>
2024-08-05 16:59:49 +0000

    Add home app and templates

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """
+    A view to return the index.html
+    """
+    return render(request, 'home/index.html')
