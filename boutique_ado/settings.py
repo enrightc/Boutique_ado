551a2b55ba65df4971a4e9753bdb9a6e3be31a61
Charles Enright <c.enright@me.com>
2024-09-03 10:17:50 +0000

    Update AWS region in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -194,7 +194,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-charlie'
-    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
@@ -214,6 +214,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
+
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
